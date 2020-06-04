e2ea08dd485088e499c2e97f5f332ff15554126c
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-06-04 14:37:24 +0300

    no tasks

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const addingForm = document.querySelector('#new-task-form');
+const tasksContainer = document.querySelector('.mb-4');
 const tasksList = document.querySelector('#tasks-list');
 
 const getRandomNumber = (min, max) => {
@@ -12,8 +13,8 @@ const makeDeleteButton = () => {
 };
 
 const createTaskTemplate = (content) => {
-    const deleteButton = makeDeleteButton();
-    const id = getRandomNumber(0, 1000);
+  const deleteButton = makeDeleteButton();
+  const id = getRandomNumber(0, 1000);
   return (
     `<li class="list-group-item d-flex justify-content-between" id="task${id}">
     <span class="task-title">${content}</span>
@@ -22,21 +23,50 @@ const createTaskTemplate = (content) => {
   );
 };
 
+const createEmptyTemplate = () => {
+  return(
+    `<div class="alert alert-dark" role="alert">
+    На сегодня нет ни одной задачи!
+    </div>`
+  );
+};
+
+const insertEmptyTask = () => {
+  const empty = createEmptyTemplate();
+    tasksContainer.insertAdjacentHTML('beforebegin', empty);
+};
+
+const checkNoTasks = () => {
+  const allTasks = document.querySelectorAll('.list-group-item ');
+  if (allTasks.length === 0) {
+    insertEmptyTask();
+  }
+};
+
+const deleteEmptyTemplate = () => {
+  const empty = document.querySelector('.alert-dark');
+  if (empty) {
+    empty.remove();
+  }
+};
+
 const addNewTask = (task) => {
   tasksList.insertAdjacentHTML('afterbegin', task);
 };
 
 const deleteTask = (id) => {
   const task = document.getElementById(id);
-  console.log(id, task);
   task.remove();
 };
+
+checkNoTasks();
 
 addingForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const inputField = addingForm.querySelector('#addNewTask');
   const content = inputField.value;
   const newTask = createTaskTemplate(content);
+  deleteEmptyTemplate();
   addNewTask(newTask);
   inputField.value = '';
 });
@@ -46,5 +76,6 @@ tasksList.addEventListener('click', (evt) => {
   if (element.getAttribute('data-action') === 'delete-task') {
     const id = element.parentElement.id;
     deleteTask(id);
+    checkNoTasks();
   }
 });
