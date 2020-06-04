152370e32d6ad84042e984eb5ba6aaf6f6c3abd9
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-06-04 14:04:51 +0300

    delete task

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
 const addingForm = document.querySelector('#new-task-form');
 const tasksList = document.querySelector('#tasks-list');
+
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
+
 const makeDeleteButton = () => {
   return (
     `<button type="button" data-action="delete-task" class="btn btn-light align-self-end">Удалить</button>`
   );
 };
+
 const createTaskTemplate = (content) => {
     const deleteButton = makeDeleteButton();
     const id = getRandomNumber(0, 1000);
   return (
-    `<li class="list-group-item d-flex justify-content-between" id="${id}">
+    `<li class="list-group-item d-flex justify-content-between" id="task${id}">
     <span class="task-title">${content}</span>
     ${deleteButton}
     </li>`
   );
 };
+
 const addNewTask = (task) => {
   tasksList.insertAdjacentHTML('afterbegin', task);
 };
-const findTaskById = (id, list) => {
-  return list.filter((item) => item.id === id);
-};
+
 const deleteTask = (id) => {
-  const allTasks = tasksList.querySelectorAll('list-group-item');
-  const task = findTaskById(allTasks, id);
-  console.log(task);
+  const task = document.getElementById(id);
+  console.log(id, task);
+  task.remove();
 };
 
 addingForm.addEventListener('submit', (evt) => {
@@ -37,4 +39,12 @@ addingForm.addEventListener('submit', (evt) => {
   const newTask = createTaskTemplate(content);
   addNewTask(newTask);
   inputField.value = '';
+});
+
+tasksList.addEventListener('click', (evt) => {
+  const element = evt.target;
+  if (element.getAttribute('data-action') === 'delete-task') {
+    const id = element.parentElement.id;
+    deleteTask(id);
+  }
 });
